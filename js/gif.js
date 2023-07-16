5eb030085be9235603e872469c4dfb729be5d71f
JimenaOcampo <jimena.a.ocampo@hotmail.com>
2023-07-16 00:26:55 -0300

    nueva actualizacion de la gifcard

--- js/gif.js
+++ js/gif.js
@@ -1,35 +1,81 @@
-let nombre = document.querySelector("#nombre");
-let fondos = document.getElementsByName("fondo");
-let color = document.getElementsByName("color");
-let ubicacion =document.getElementsByName("posi");
-let vistaPrevia = document.querySelector(".vista-previa");
-let texto = document.querySelector("#texto");
 
-//escucho el keyup porque quiero soltar teclado
-//usar en texto sobre escrito en algo
+let vistaPrevia = document.querySelector(".vista-previa");
+let vistaPreviaprecio = document.querySelector(".vista-previa-precio");
+let monto=document.querySelector("#monto");
+let nombre = document.querySelector("#nombre");
+
+/* estas vienen de un  <label for="algo"></label> que siguen a un name="algo" */
+let tamaño_fuente = document.getElementsByName("tamaño");
+let color = document.getElementsByName("colorfuente");
+
+let ubicacion = document.getElementsByName("posi");
+let fondos = document.getElementsByName("fondo");
+
+
+//CAMBIAR MONTO
+monto.addEventListener("keyup", () => {
+    vistaPreviaprecio.innerHTML = `$ ${monto.value}.-`;
+  }); 
+
 nombre.addEventListener("keyup", () => {
     //agregar a vista previa el valor del imput nombre
-    vistaPrevia.innerHTML = nombre.value;
+    vistaPrevia.innerHTML = `GIFCARD PARA: ${nombre.value}`;
+
 
 });
 
-//usar en los imput
-//usar en los imput
+
 fondos.forEach((item) => {
     item.addEventListener("click", () => {
         //console.log(item.value);
-        vistaPrevia.classList.remove("fondo1", "fondo2", "fondo3","fondo4", "fondo5", "fondo6");
+        vistaPrevia.classList.remove(
+            "fondo1",
+            "fondo2",
+            "fondo3",
+            "fondo4",
+            "fondo5",
+            "fondo6");
         vistaPrevia.classList.add(`fondo${item.value}`);
     });
 
 });
 
-//color de la letra seleccionable
+
 color.forEach((item) => {
     item.addEventListener("click", () => {
         //console.log(item.value);
-        vistaPrevia.classList.remove("blanco", "negro");
-        vistaPrevia.classList.add(item.value);
+        vistaPrevia.classList.remove(
+            "colorfuente1",
+            "colorfuente2",
+            "colorfuente3",
+            "colorfuente4",
+            "colorfuente5");
+            vistaPrevia.classList.add(`colorfuente${item.value}`);
 
+    });
+});
+
+
+tamaño_fuente.forEach((item) => {
+    item.addEventListener("click", () => {
+        vistaPrevia.classList.remove(
+            "tamaño1",
+            "tamaño2",
+            "tamaño3",
+            "tamaño4",
+            "tamaño5");
+        vistaPrevia.classList.add(`tamaño${item.value}`);
+    });
+});
+
+
+
+ubicacion.forEach((item) => {
+    item.addEventListener("click", () => {
+        vistaPreviaprecio.classList.remove(
+            "posi1",
+            "posi2",
+            "posi3");
+            vistaPreviaprecio.classList.add(`posi${item.value}`);
     });
 });
